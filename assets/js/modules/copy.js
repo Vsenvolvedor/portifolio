693fc7c5f14f0fb271e1bb5349cedb57cfd26a7f
Vitor <catchtherainbow33@gmail.com>
2022-04-07 15:10:24 -0300

    adicionado link para o codigo

--- assets/js/modules/copy.js
+++ assets/js/modules/copy.js
@@ -1,23 +1,23 @@
 export default class Copy {
   
-  constructor(copySelect,activeClass){
-    this.copySelect = document.querySelector(copySelect)
-    this.activeClass = activeClass
+  constructor(copySelect,activeClass) {
+    this.copySelect = document.querySelector(copySelect);
+    this.activeClass = activeClass;
 
-    this.copyToBoard = this.copyToBoard.bind(this)
+    this.copyToBoard = this.copyToBoard.bind(this);
   }
   
   copyToBoard() { 
-    const copy = this.copySelect.dataset.copy
-    this.copySelect.classList.add(this.activeClass)
-    navigator.clipboard.writeText(copy)
-    setTimeout(()=>{this.copySelect.classList.remove(this.activeClass)},1000)
+    const copy = this.copySelect.dataset.copy;
+    this.copySelect.classList.add(this.activeClass);
+    navigator.clipboard.writeText(copy);
+    setTimeout(()=>{this.copySelect.classList.remove(this.activeClass)},1000);
   }
 
-  addClickEvent(){this.copySelect.addEventListener('click', this.copyToBoard)}
+  addClickEvent() {this.copySelect.addEventListener('click', this.copyToBoard);}
 
   init(){
-    this.addClickEvent()
-    return this
+    this.addClickEvent();
+    return this;
   }
 }
